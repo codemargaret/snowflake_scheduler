2fbf7b7b07c10abf2c3753a99d3bcd696e9bc471
daniel garcia and Dan Kiss <daniel@>
2017-10-02 16:12:07 -0700

    add passing specs for datatables

--- db/migrate/20171002204836_drop_member_availabilities_and_add_member_avails.rb
+++ db/migrate/20171002204836_drop_member_availabilities_and_add_member_avails.rb
@@ -1,7 +1,7 @@
 class DropMemberAvailabilitiesAndAddMemberAvails < ActiveRecord::Migration[5.1]
   def change
     drop_table(:member_availabilities)
-    
+
     create_table(:member_avails) do |t|
       t.column(:day_id, :int)
       t.column(:member_id, :int)
