ee812d41e99ca5a7626894751118efe373cc260b
Daniel Garcia and Margaret Berry <daniel@>
2017-10-03 11:36:01 -0700

    add integration specs for group and member

--- spec/group_integration_spec.rb
+++ spec/group_integration_spec.rb
@@ -1,1 +1,34 @@
 require 'spec_helper'
+
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe 'the group creation path', {:type => :feature} do
+  it 'takes the user to a homepage where they can add groups' do
+    visit '/'
+    click_link('Create New Group')
+    fill_in('group_name', :with => 'One Way Train')
+    click_button('Create!')
+    expect(page).to have_content('One Way Train')
+  end
+end
+
+describe 'the group update path', {:type => :feature} do
+  it 'allows the user to change the name of a group' do
+    group = Group.create({:name => 'One Way Train'})
+    visit '/'
+    click_link('edit')
+    fill_in('new_group_name', :with => 'One Way Plane')
+    click_button('Update Group')
+    expect(page).to have_content('One Way Plane')
+  end
+end
+
+describe 'the group delete path', {:type => :feature} do
+  it 'allows a user to delete a group' do
+    group = Group.create({:name => 'One Way Train'})
+    visit '/'
+    click_button('Delete Group')
+    expect(page).not_to have_content("One Way Train")
+  end
+end
--- spec/member_integration_spec.rb
+++ spec/member_integration_spec.rb
@@ -1,1 +1,34 @@
 require 'spec_helper'
+
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe 'the member creation path', {:type => :feature} do
+  it 'takes the user to a homepage where they can add members' do
+    visit '/'
+    click_link('Create New Member')
+    fill_in('member_name', :with => 'Dan')
+    click_button('Create!')
+    expect(page).to have_content('Dan')
+  end
+end
+
+describe 'the member update path', {:type => :feature} do
+  it 'allows the user to change the name of a member' do
+    member = Member.create({:name => 'Dan'})
+    visit '/'
+    click_link('edit')
+    fill_in('new_member_name', :with => 'Dan Garcia')
+    click_button('Update Member')
+    expect(page).to have_content('Dan Garcia')
+  end
+end
+
+describe 'the member delete path', {:type => :feature} do
+  it 'allows a user to delete a member' do
+    member = Member.create({:name => 'Dan'})
+    visit '/'
+    click_button('Delete Member')
+    expect(page).not_to have_content("Dan")
+  end
+end
