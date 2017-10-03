44d41a25f80d92f54b4ea93e358ab36da1292a25
Michael Brooks and Margaret Berry <margaret@>
2017-10-02 17:14:13 -0700

    add member create, get, and post

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/')do
   @groups = Group.all
   @members = Member.all
+  binding.pry
   erb(:index)
 end
 
@@ -28,12 +29,23 @@ post('/group/create') do
   end
 end
 
-
-
-
-
 #MEMBER##############################
+get('/member/create') do
+  @members = Member.all
+  erb(:create_member)
+end
 
+post('/member/create') do
+  group_ids = params['group_ids']
+  name = params['name']
+  if Member.create({:group_ids => group_ids, :name => name})
+    redirect('/')
+  else
+    @not_saved = true
+    @members = Member.all
+    erb(:create_member)
+  end
+end
 
 
 
