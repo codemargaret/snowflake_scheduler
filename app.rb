2f95a4cd9740a8c4ab3b9ad6fb788605fa070123
Daniel Garcia and Margaret Berry <daniel@>
2017-10-03 10:35:56 -0700

    add ability to create and show members and groups

--- app.rb
+++ app.rb
@@ -7,20 +7,19 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/')do
   @groups = Group.all
   @members = Member.all
-  binding.pry
   erb(:index)
 end
 
 #GROUP##############################
 get('/group/create') do
-  @groups = Group.all
+  @members = Member.all
   erb(:create_group)
 end
 
 post('/group/create') do
   member_ids = params['member_ids']
-  name = params['name']
-  if Group.create({:member_ids => member_ids, :name => name})
+  name = params['group_name']
+  if Group.create({:meetup_id => member_ids, :name => name})
     redirect('/')
   else
     @not_saved = true
@@ -31,13 +30,13 @@ end
 
 #MEMBER##############################
 get('/member/create') do
-  @members = Member.all
+  @groups = Group.all
   erb(:create_member)
 end
 
 post('/member/create') do
   group_ids = params['group_ids']
-  name = params['name']
+  name = params['member_name']
   if Member.create({:group_ids => group_ids, :name => name})
     redirect('/')
   else
