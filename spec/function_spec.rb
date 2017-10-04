f7a400b786e49916a001e267da86dce2442d1f24
Dan Kiss and Margaret Berry <dan@>
2017-10-04 12:22:51 -0700

    clean up files

--- spec/function_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-# monday_day1 = Day.create(:name => 'Monday')
-# monday_day = Day.find_by(name: 'Monday')
-# member1 = Member.create(:name => 'steve', :role_id => nil)
-# member1_id = member1.id
-# member_avails_row1 = MemberAvail.create(:day_id => monday_day.id, :member_id => member1.id)
