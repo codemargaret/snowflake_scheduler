bbb41551d70dd25117de91ed99a1598b58306a68
Dan Kiss and Margaret Berry <dan@>
2017-10-04 13:23:54 -0700

    add way to scale self.find_avail function

--- spec/general_spec.rb
+++ spec/general_spec.rb
@@ -1,15 +1,49 @@
 require('spec_helper')
 
 describe(Day) do
+  # it 'checks member availibility by day' do
+  #   monday_day1 = Day.create(:name => 'Monday')
+  #   monday_day = Day.find_by(name: 'Monday')
+  #   member1 = Member.create(:name => 'steve', :role_id => nil)
+  #   member2 = Member.create(:name => 'mike', :role_id => nil)
+  #   member_avails_row1 = MemberAvail.create(:day_id => monday_day.id, :member_id => member1.id)
+  #   member_avails_row2 = MemberAvail.create(:day_id => monday_day.id, :member_id => member2.id)
+  #   # binding.pry
+  #   expect(MemberAvail.find_mon_avail).to(eq(["steve", "mike"]))
+  # end
+
   it 'checks member availibility by day' do
     monday_day1 = Day.create(:name => 'Monday')
     monday_day = Day.find_by(name: 'Monday')
     member1 = Member.create(:name => 'steve', :role_id => nil)
+    member2 = Member.create(:name => 'mike', :role_id => nil)
     member_avails_row1 = MemberAvail.create(:day_id => monday_day.id, :member_id => member1.id)
+    member_avails_row2 = MemberAvail.create(:day_id => monday_day.id, :member_id => member2.id)
     # binding.pry
-    expect(MemberAvail.find_mon_avail).to(eq(["steve"]))
+    expect(MemberAvail.find_avail('Monday')).to(eq(["steve", "mike"]))
   end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 describe(MemberAvail) do
